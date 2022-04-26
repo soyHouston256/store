a3ed60d7fd3df29f1d013c66c9263e1e3168861c
Max Houston Ramirez Martel <maxr524@gmail.com>
2022-04-25 22:05:04 -0500

    file structure

---
+++ src/infrastructure/components/Home.tsx
@@ -0,0 +1,14 @@
+import React form 'react'
+
+class HomeComponent extends React.Component {
+    constructor () {
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <span>Hola!, soy un componente</span>
+            </div>
+        );
+    }
+}   
